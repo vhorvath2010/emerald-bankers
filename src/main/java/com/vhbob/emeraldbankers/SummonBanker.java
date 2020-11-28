6a9adb202d3071cb79015a452a0e3b8694a550e9
vhorvath2010 <vhorvath2010@gmail.com>
2020-11-28 11:43:36 -0500

    Committing to switch PCs

--- src/main/java/com/vhbob/emeraldbankers/SummonBanker.java
+++ src/main/java/com/vhbob/emeraldbankers/SummonBanker.java
@@ -1,4 +1,15 @@
 package com.vhbob.emeraldbankers;
 
-public class SummonBanker {
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class SummonBanker implements CommandExecutor {
+
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (command.getName().equalsIgnoreCase("summonBanker")) {
+
+        }
+        return false;
+    }
 }
