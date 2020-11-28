f4045e3edc561e02dd9e1b8446876ebbd92b1d69
vhorvath2010 <vhorvath2010@gmail.com>
2020-11-28 11:41:47 -0500

    Initial commit

---
+++ src/main/java/com/vhbob/emeraldbankers/EmeraldBankers.java
@@ -0,0 +1,12 @@
+package com.vhbob.emeraldbankers;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class EmeraldBankers extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        saveDefaultConfig();
+    }
+    
+}
